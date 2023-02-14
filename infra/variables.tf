855ec8a7330f3aacdf8a34d2aeaccb632efc75eb
Pedro <p-cardozo@hotmail.com>
2023-02-14 20:29:44 -0300

    feat(varaibles): including variables for ec2 type and name

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,15 @@ variable "private_key_path" {
   type    = string
   default = "/usr/my_key.pem"
 }
+
+variable "instance_type" {
+  type    = string
+  default = "t3.nano"
+  description = "The instance type from the EC2 machine"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "ec2_3dol"
+  description = "The instance name from the EC2 machine"
+}
