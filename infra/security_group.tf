a844986c0893fed911891edf81a09f8c77c58c3c
Pedro <p-cardozo@hotmail.com>
2023-02-14 21:32:48 -0300

    feat(sg): including sg file and using ec2 module

---
+++ infra/security_group.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "sg_ec2_3dol" {
+  name_prefix = "example"
+  
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
