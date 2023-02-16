eb4c04acfc927df9efcd59b97318664745919344
Pedro <p-cardozo@hotmail.com>
2023-02-15 21:04:48 -0300

    feat(ec2): updating filename

--- infra/terraform/local_file_ansible.tf
+++ infra/terraform/local_file_ansible.tf
@@ -1,11 +1,11 @@
 resource "local_file" "server_ec2_3dol_key" {
   content         = tls_private_key.ssh.private_key_pem
-  filename        = "${path.module}/infra/ansible/ec2_3dol.pem"
+  filename        = "${path.module}/../../infra/ansible/ec2_3dol.pem"
   file_permission = "0600"
 }
 
 resource "local_file" "ansible_hosts" {
-  filename = "${path.module}/infra/ansible/hosts"
+  filename = "${path.module}/../../infra/ansible/hosts"
   content  = <<EOF
   [ec2]
   ${aws_eip.ec2_eip.public_ip}
